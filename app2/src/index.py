11fa5b3be93d948c2e7135b005442a4746828010
Sergio Méndez <sergio.mendez@yalo.com>
2022-02-20 12:43:53 -0600

    feat: app2 changed for json output

--- app2/src/index.py
+++ app2/src/index.py
@@ -1,13 +1,17 @@
-from flask import Flask
-import os
-import socket
+from flask import Flask 
+from flask import jsonify 
+import os 
+import socket 
 
-app = Flask(__name__)
+app = Flask(__name__) 
+
+host = socket.gethostname() 
+
+msg = os.environ['MESSAGE'] 
 
 @app.route('/')
-def index():
-    return "Host:"+socket.gethostname()+" "+os.environ['MESSAGE']
+def index(): 
+    return jsonify({"host":host,"msg":msg}) 
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=5000, debug=True)
