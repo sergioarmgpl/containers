cbf1a6c19532588eb0d34f69b60048f786533d11
Sergio Méndez <sergio.mendez@yalo.com>
2022-03-31 00:18:19 -0600

    feat: updated exporter container

--- exporter/src/build_push.sh
+++ exporter/src/build_push.sh
@@ -1,2 +1,2 @@
 docker login
-docker buildx build --platform linux/arm64 -t $1/app2prom --push .
+docker buildx build --platform linux/arm64 -t $1/exporter --push .
--- exporter/src/index.py
+++ exporter/src/index.py
@@ -1,11 +1,12 @@
 from flask import Response, Flask, request, jsonify
-
 import prometheus_client
 from prometheus_client.core import CollectorRegistry
 from prometheus_client import Gauge
 import time
 import redis
 import os
+import sys
+import json
 
 app = Flask(__name__)
 t = Gauge('weather_metric1', 'temperature')
@@ -18,15 +19,16 @@ r = redis.StrictRedis(host=rhost,\
         port=6379,db=0,password=rauth,\
         decode_responses=True)
 
-
 @app.route("/metrics")
 def metrics():
-    data = int(r.lpop(stopic,msg.payload))
-    t.set(int(temperature))
-    h.set(int(humidity))
+    data = r.lpop(stopic)
+    values = json.loads(str(data).replace("\'","\""))
+    t.set(int(values["temperature"]))
+    h.set(int(values["humidity"]))
     res = []
     res.append(prometheus_client.generate_latest(t))
     res.append(prometheus_client.generate_latest(h))
+    print({"processed":true},file=sys.stderr)
     return Response(res, mimetype="text/plain")
 
 if __name__ == '__main__':
