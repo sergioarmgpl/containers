d26cb0e15f787ec11e9f89b34b5d5c196ac1a1f6
Sergio Méndez <sergio.mendez@yalo.com>
2022-05-20 01:03:58 -0600

    feat: added gps-server & gps-reader

---
+++ gps-reader/src/index.py
@@ -0,0 +1,26 @@
+import serial
+import time
+import string
+import pynmea2
+import os
+import requests
+import sys
+
+while True:
+   device = os.environ['DEVICE']
+   ser=serial.Serial(device, baudrate=9600, timeout=0.5)
+   dataout = pynmea2.NMEAStreamReader()
+   newdata=ser.readline().decode('utf-8')
+
+   if newdata[0:6] == "$GPRMC":
+      newmsg=pynmea2.parse(newdata)
+      lat = newmsg.latitude
+      lng = newmsg.longitude
+      cid = os.environ['CLIENT_ID']
+      data = {'lat': lat,'lng':lng,'cid':cid}
+      headers={"Content-Type":"application/json"}
+      r = requests.post(os.environ['ENDPOINT']
+        + f"/client/{cid}/position",json=data)
+      print(str(r),file=sys.stderr)
+      print(str(data),file=sys.stderr)
+
