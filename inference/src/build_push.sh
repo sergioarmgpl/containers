2cdec6c1bd14d09412030e056ac4a1392f82a420
Sergio Méndez <sergio.mendez@yalo.com>
2022-07-02 22:38:26 -0600

    feat: feat added inference arm/v7

--- inference/src/build_push.sh
+++ inference/src/build_push.sh
@@ -1,5 +1,2 @@
 docker login
-#docker buildx build --platform linux/arm64 -t $1/inference --push .
-#docker buildx build --platform linux/arm/v7 -t $1/inference --push .
-docker build -t $1/inference
-docker push $1/inference
+docker buildx build --platform linux/arm/v7 -t $1/inference --push .
