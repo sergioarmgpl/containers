1eadbfc6441757fea5fe9bde209bfa197d5a9864
Sergio Méndez <sergio.mendez@yalo.com>
2022-07-10 00:30:15 -0600

    feat: mod traffic-manager and gps-api to evaluate similar GPS coordinates

--- gps-api/src/index.py
+++ gps-api/src/index.py
@@ -1,16 +1,15 @@
 import os
 import requests
 import sys
-
 from flask import Flask, request
 from flask import jsonify
 from flask_cors import CORS
 import json
 from datetime import datetime
+from geopy import distance
 
 queue = []
 traffic_events = []
-cid = "cid-"+str(int(datetime.timestamp(datetime.now())))
 
 app = Flask(__name__)
 CORS(app,origins=["*"])
@@ -23,26 +22,40 @@ def registerTrafficEvent():
    coor = json.loads(raw)
    f.close()
    data = json.loads(request.data)
+   ts = str(int(datetime.timestamp(datetime.now())))
    event = {'lat': coor["lat"],
          'lng':coor["lng"],
+         'ts':ts,
          'object':data["object"],
          'warning':data["warning"]}
    print(event,file=sys.stderr)
    traffic_events.append(event)
-   return jsonify({"client_id":cid,"registerTrafficEvent":"done"})
+   return jsonify({"client_id":data["object"]+"-"+ts,"registerTrafficEvent":"done"})
 
 @app.route("/traffic", methods=["GET"])
 def syncTrafficEvents():
    global traffic_events
    headers={"Content-Type":"application/json"}
    print(traffic_events,file=sys.stderr)
+   tf_tmp = traffic_events
+   while len(tf_tmp) > 0:
+      for i in range(0,len(tf_tmp)):
+         if i+1 <= len(tf_tmp):
+            for j in range(i+1,len(tf_tmp)):
+               m = distance.distance(
+                  (tf_tmp[i]["lat"],tf_tmp[i]["lng"]),
+                  (tf_tmp[i]["lat"],tf_tmp[i]["lng"]),
+               ).m
+               if m < 5:
+                  del tf_tmp[j]
+   print("Filtered",tf_tmp,file=sys.stderr)
    r = requests.post(os.environ['ENDPOINT']
    + f"/traffic/1",json=
    {
-      "data":traffic_events
+      "data":tf_tmp
    })
    traffic_events.clear()
-   return jsonify({"client_id":cid,"syncTrafficEvents":"done"})
+   return jsonify({"syncTrafficEvents":"done"})
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=3000, debug=True)
--- traffic-manager/src/index.py
+++ traffic-manager/src/index.py
@@ -28,13 +28,20 @@ def setBulkTrafficObjects():
     data = request.json
     warnings = data["data"]
     for w in warnings:
-        object_name = w["object"]+"-"+str(int(time.time()))
-        r.geoadd(f"traffic",[float(w["lng"]),float(w["lat"]),\
-        object_name],ch=True)
-        r.hset(f"object:{object_name}:data","type",w["object"])
-        r.hset(f"object:{object_name}:data","warning",w["warning"])
-        r.expire("traffic",ttl_trf)
-        r.expire(f"object:{object_name}:data",ttl_obj)
+        object_name = w["object"]+"-"+w["ts"]
+        print({"inserting":object_name},file=sys.stderr)
+
+        similar = r.geosearch("traffic",unit="m",radius=5.0,
+        longitude = float(w["lng"]),latitude=float(w["lat"]),sort="ASC")
+        
+        if len(similar) == 0:
+            r.geoadd(f"traffic",[float(w["lng"]),float(w["lat"]),\
+            object_name],ch=True)
+            r.hset(f"object:{object_name}:data","type",w["object"])
+            r.hset(f"object:{object_name}:data","warning",w["warning"])
+            r.expire("traffic",ttl_trf)
+            r.expire(f"object:{object_name}:data",ttl_obj)
+            print({"found":"similar GPS coordinates"},file=sys.stderr)
     return jsonify({"setTrafficObject":"done"})
 
 @app.route("/traffic/2", methods=["POST"])
@@ -43,11 +50,12 @@ def setSingleTrafficObject():
     r = redisCon()
     data = request.json
     pos = data["position"]
-    object_name = data["object"]+"-"+str(int(time.time()))
+    object_name = w["object"]+"-"+w["ts"]
     r.geoadd(f"traffic",[float(pos["lng"]),float(pos["lat"]),\
     object_name],ch=True)
     r.hset(f"object:{object_name}:data","type",data["object"])
     r.hset(f"object:{object_name}:data","warning",data["warning"])
+    r.hset(f"object:{object_name}:data","ts",data["ts"])
     r.expire("traffic",ttl_trf)
     r.expire(f"object:{object_name}:data",ttl_obj)
     return jsonify({"setTrafficObject":"done"})
@@ -63,12 +71,13 @@ def getTrafficObjects(unit,radius,lat,lng):
         pos = r.geopos("traffic",obj)
         #get warning and type metadata
         obj_data = r.hgetall(f"object:{obj}:data")
-        data.append({"object":obj,
+        data.append({
+        "object":obj_data["type"]+"-"+obj_data["ts"],
         "lat":pos[0][0],
         "lng":pos[0][1],
         "type":obj_data["type"],
-        "warning":obj_data["warning"],
-        "object":obj})    
+        "warning":obj_data["warning"]
+        })    
 
     return jsonify({"getTrafficObjects":"done",
     "objects":data
